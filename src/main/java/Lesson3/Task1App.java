37a23cadb159dc9e1b5fbfb6d955cdf624e94454
IvanZharov <ivan-stud@yandex.ru>
2021-10-19 17:30:57 +0300

    Все работает нормально.

---
+++ src/main/java/Lesson3/Task1App.java
@@ -0,0 +1,55 @@
+package Lesson3;
+
+import java.util.*;
+
+public class Task1App {
+    public static void main(String[] args) {
+        String[] basicWordsList = {
+                "A", "B", "j", "D", "E",
+                "F", "A", "C", "D", "E",
+                "E", "A", "C", "A", "A"
+        };
+        findUnique(basicWordsList);
+
+    }
+/* Создаем копию базового массива.
+В копии удаляем все повторяющиеся элементы.
+Создаем мапу, в которую добавляем все элементы из нашей копии как уникальные ключи.
+Считаем кол-во строк текущего ключа в базовом массиве - это значение мапы.
+
+ */
+
+    public static String[] findUnique(String[] basic) {
+        String[] workList = new String[basic.length];
+        for (int i = 0; i < basic.length; i++) {
+            workList[i] = basic[i];
+        }
+        for (int i = 0; i < basic.length-1; i++) {
+            for (int j = i + 1; j < basic.length; j++) {
+                if (basic[i].equals(basic[j])) {
+                    workList[j] = null;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(workList));
+
+        Map<String, Integer> uniqueWordsCounter = new HashMap<>();
+        for (int i = 0; i < basic.length; i++) {
+            if (workList[i] != null) {
+                int counter = 0;
+                for (int j = 0; j < basic.length; j++) {
+                    if (basic[j].equals(workList[i])) {
+                        counter += 1;
+                    }
+                }
+                uniqueWordsCounter.put(workList[i], counter);
+            }
+        }
+        for (Map.Entry<String, Integer> pair : uniqueWordsCounter.entrySet()) {
+            String key = pair.getKey();
+            int value = pair.getValue();
+            System.out.println(key + " повторяется " + value + " раз.");
+        }
+        return workList;
+    }
+}
---
+++ src/main/java/Lesson3/Task2App.java
@@ -0,0 +1,38 @@
+package Lesson3;
+
+import java.sql.Array;
+import java.util.*;
+
+public class Task2App {
+    static Map<String, String> phoneBook = new HashMap<>();
+
+    public static void main(String[] args) {
+        add("123-32-56","Иванов");
+        add("123-32-30","Петров");
+        add("123-32-40","Иванов");
+        add("123-32-70","Сидоров");
+        add("123-32-68","Иванов");
+        add("123-32-00","Иванов");
+
+        get("Иванов");
+
+    }
+
+    private static void add(String number, String name) {
+        phoneBook.put(number, name);
+        System.out.println("Added");
+    }
+
+    private static void get(String name) {
+        List<String> neededNumbers = new ArrayList<>();
+        for (Map.Entry<String, String> pair : phoneBook.entrySet()) {
+            String value = pair.getValue();
+            if (value.equals(name)) {
+                neededNumbers.add(pair.getKey());
+            }
+        }
+        System.out.println(name + " имеет следующие телефонные номера: ");
+        System.out.println(neededNumbers);
+    }
+}
+
