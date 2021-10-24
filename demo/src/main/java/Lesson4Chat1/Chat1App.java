8ddaa52c89b45fd47362da53b8562d185bd4d594
IvanZharov <ivan-stud@yandex.ru>
2021-10-24 17:53:16 +0300

    Заглушка под дз

---
+++ demo/src/main/java/Lesson4Chat1/Chat1App.java
@@ -0,0 +1,16 @@
+package Lesson4Chat1;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Chat1App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
